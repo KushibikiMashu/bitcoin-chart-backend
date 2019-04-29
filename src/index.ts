20ea4a192042fe2f8f2fb5da6f04cf8d3105586f
KushibikiMashu <kushibiki3413@gmail.com>
2019-04-29 23:36:07 +0900

    index.tsに型をつける

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,9 @@
-const express = require('express')
-const app = express()
-const port = 3005
+import * as express from "express";
+const app = express();
+const port = 3005;
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.get("/", (req: express.Request, res: express.Response) =>
+  res.send("Hello World!")
+);
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
