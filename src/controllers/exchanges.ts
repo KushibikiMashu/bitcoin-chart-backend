69d974f5277edb45d9fdfa5cc3851e5e60af7023
KushibikiMashu <kushibiki3413@gmail.com>
2019-04-30 09:42:10 +0900

    構成の変更

--- src/controllers/exchanges.ts
+++ src/controllers/exchanges.ts
@@ -4,7 +4,7 @@ import { set } from "../models/zaifModel";
 
 const router = express.Router();
 
-router.get("/list", (req: express.Request, res: express.Response) => {
+router.get("/chart", (req: express.Request, res: express.Response) => {
   // dbから値を取り出す
   // jsonにして返却する
   set();
--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,13 @@
-import * as express from 'express'
-import api from './controllers/api'
+import * as express from "express";
+import exchanges from "./controllers/exchanges";
 
-const app = express()
-const port = 3005
+const app = express();
+const port = 3005;
 
-app.use('/api', api)
+app.get("/", (req: express.Request, res: express.Response) =>
+  res.sendStatus(404)
+);
 
-app.get('/', (req: express.Request, res: express.Response) => res.status(404).json({ message: 'File Not Found.' }))
+app.use("/api/exchanges", exchanges);
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
