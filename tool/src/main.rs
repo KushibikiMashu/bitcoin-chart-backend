4d32a2882efa2008fff83a42539c1ea8ca03698c
KushibikiMashu <kushibiki3413@gmail.com>
2019-05-02 22:51:33 +0900

    csvの読み込みと新しい値の作成

--- tool/src/main.rs
+++ tool/src/main.rs
@@ -1,40 +1,50 @@
 extern crate csv;
 extern crate chrono;
+extern crate serde;
+extern crate serde_json;
+#[macro_use]
+extern crate serde_derive;
 
 use std::{
     error::Error,
     process,
+    collections::HashMap,
 };
-
 use csv::ReaderBuilder;
+use chrono::NaiveDateTime;
+use serde::{Serialize, Deserialize};
 
-use chrono::{
-    NaiveDateTime,
-};
+#[derive(Debug, Deserialize)]
+struct Record {
+    id: i32,
+    price: i32,
+    created_at: String,
+}
 
+#[derive(Debug)]
 struct BitcoinRecord {
     id: i32,
     price: i32,
     timestamp: i64,
-    created_at: str,
+    created_at: String,
 }
 
-//impl BitcoinRecord {
-//    pub fn new(record: csv::StringRecord) -> BitcoinRecord {
-//        BitcoinRecord {
-//            id: record[0],
-//            price: record[1],
-//            timestamp: str_to_timestamp(&record[2]),
-//            created_at: record[2],
-//        }
-//    }
-//}
-//
-//fn str_to_timestamp(datetime: &str) -> i64 {
-//    let dt = NaiveDateTime::parse_from_str(datetime, "%Y-%m-%d %H:%M:%S").unwrap();
-//    let mut timestamp: i64 = dt.timestamp();
-//    (timestamp - 9 * 60 * 60)
-//}
+impl BitcoinRecord {
+    pub fn new(record: Record) -> BitcoinRecord {
+        BitcoinRecord {
+            id: record.id,
+            price: record.price,
+            timestamp: str_to_timestamp(&record.created_at),
+            created_at: record.created_at,
+        }
+    }
+}
+
+fn str_to_timestamp(datetime: &String) -> i64 {
+    let dt = NaiveDateTime::parse_from_str(datetime, "%Y-%m-%d %H:%M:%S").unwrap();
+    let mut timestamp: i64 = dt.timestamp();
+    (timestamp - 9 * 60 * 60)
+}
 
 
 fn read(filenames: [&str; 3]) -> Result<(), Box<Error>> {
@@ -43,12 +53,10 @@ fn read(filenames: [&str; 3]) -> Result<(), Box<Error>> {
             .has_headers(false)
             .from_path(filename)?;
 
-        for result in rdr.records() {
-            let record = result?;
-            println!("{:?}", record);
-
-//            let b_record: BitcoinRecord = BitcoinRecord::new(record);
-//            println!("{:?#}", b_record);
+        for result in rdr.deserialize() {
+            let record: Record = result?;
+            let b_record: BitcoinRecord = BitcoinRecord::new(record);
+            println!("{:?}", b_record);
         }
     }
     Ok(())
